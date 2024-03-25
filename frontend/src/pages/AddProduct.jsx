f532f72bbc8001ae33e6d83f2e4ba35aab5bfc54
unknown <eddmyconpeopoenterinabacotot@gmail.com>
2024-03-25 19:50:34 +0800

    Added Collor to burron

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import './style.css'
 
 export default function AddProduct() {
     const navigate = useNavigate();
@@ -64,7 +65,7 @@ export default function AddProduct() {
         <>            
 			<div className='min-h-screen mt-20'>
 			<div className='flex p-3 max-w-3xl mx-auto text-center	 flex-row md:flex-col md:items-center gap-5'> 
-				<Form className='border border shadow' style={{ width: "30%" }} onSubmit={e => createProduct(e)}>
+				<Form className='border border shadow ' style={{ width: "30%" }} onSubmit={e => createProduct(e)}>
 				<Form.Group className='pt-3' controlId="productName">
 					<Form.Label className='mt-2'>Name:</Form.Label>
 					<Form.Control type="text" placeholder="Product Name" required value={name} onChange={e => { setName(e.target.value) }} />
@@ -77,7 +78,31 @@ export default function AddProduct() {
 					<Form.Label className='mt-2'>Price:</Form.Label>
 					<Form.Control type="number" placeholder="Enter Price" required value={price} onChange={e => { setPrice(e.target.value) }} />
 				</Form.Group>
-				<Button variant="primary" type="submit" className="my-5 border rounded shadow p-2 text-white" style={{ border: "1px solid #007bff", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)", backgroundColor: "#007bff" }}>Submit</Button>
+				<Button 
+                    type="submit" 
+                    className='border rounded shadow p-2' 
+                    style={{
+                        background: 'linear-gradient(to right, blue, pink)',
+                        outline: 'linear-gradient(to right, blue, pink)'
+                    }}
+                    onMouseEnter={(e) => {
+                        
+                        e.target.style.background = 'linear-gradient(to right, pink, blue)'; // Removing background on mouse leave
+                        e.target.style.outline = 'none'; // Removing outline on mouse leave
+                        e.target.style.color = 'white';
+                    }}
+                    onMouseLeave={(e) => {
+                        e.target.style.background = 'none'; // Removing background on hover
+                        e.target.style.outline = '1px solid blue'; // Adding outline on hover
+                        e.target.style.color = 'black';
+                    }}
+                >
+                    Submit
+                </Button>
+
+
+
+
 				</Form>
 			</div>
 			</div>
